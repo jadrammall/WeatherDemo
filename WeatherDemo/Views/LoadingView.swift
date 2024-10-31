de7876ec2aa0f2cde8050bf3cf77042b00b7ca6b
jadrammall <jrammal12@gmail.com>
2024-10-31 11:18:44 +0200

    Initial Commit

---
+++ WeatherDemo/Views/LoadingView.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingView.swift
+//  WeatherDemo
+//
+//  Created by Jad Rammal on 31/10/2024.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
