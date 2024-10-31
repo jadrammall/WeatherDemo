9cc12738d1dc0707b28874b9c789f23ca4d6b428
jadrammall <jrammal12@gmail.com>
2024-10-31 10:16:53 +0200

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Jad Rammal on 31/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
